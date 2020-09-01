86b9768d92df0a0a130e85b89f71ba84576596e8
sami kling <sakling@live.com>
2020-09-01 10:45:31 +0300

    valitallennus

---
+++ WebAppFirst/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            NorthwindEntities db = new NorthwindEntities(); //Uusi ilmentymä tietokannasta nimellä db
+            List<Products> model = db.Products.ToList();    //Lista nimeltä model, joka ottaa vastaan Products kategoriaan kuuluvia rivejä, jotka sijoitetaan db oliosta listana.
+            db.Dispose();                                   //Suljetaan tietokanta yhteys
+
+            return View(model);                             //Palautetaan näkymälle lista model.
+        }
+    }
+}
