ee481620f501bd14e2e79c28d13ee25404a92d08
sami kling <sakling@live.com>
2020-10-16 19:10:48 +0300

    Created a backup. Installed PagedLists.MVC from Nuget and updated Bootstrap to the latest version.

---
+++ WebAppFirst - Backup/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+           NorthwindEntities  db = new NorthwindEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivull
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Tietoa minusta:";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Täältä löydät yhteystietoni.";
+
+            return View();
+        }
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Saapumisohje";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppFirst - Backup/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+            NorthwindEntities db = new NorthwindEntities(); //Uusi ilmentymä tietokannasta nimellä db
+        public ActionResult Index()
+        {
+            List<Products> model = db.Products.ToList();    //Lista nimeltä model, joka ottaa vastaan Products kategoriaan kuuluvia rivejä, jotka sijoitetaan db oliosta listana.
+            db.Dispose();                                   //Suljetaan tietokanta yhteys
+
+            return View(model);                             //Palautetaan näkymälle lista model.
+        }
+
+        public ActionResult Index2()
+        {
+            NorthwindEntities db = new NorthwindEntities(); //Uusi ilmentymä tietokannasta nimellä db
+            List<Products> model = db.Products.ToList();    //Lista nimeltä model, joka ottaa vastaan Products kategoriaan kuuluvia rivejä, jotka sijoitetaan db oliosta listana.
+            db.Dispose();                                   //Suljetaan tietokanta yhteys
+
+            return View(model);                             //Palautetaan näkymälle lista model.
+        }
+        public ActionResult Details(int? id) //parametri tutkitaan ja etsitään taulusta vastaavaa riviä. Jos vastaavuus löytyy, Näytetään Edit.cshtml sivu.
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                Products tuote = db.Products.Find(id);
+                if (tuote == null) return HttpNotFound(); //Jos vastaavuutta ei löydy, palautetaan sivua ei löydy virhe.
+                return View(tuote);
+            }
+        }
+    }
+}
